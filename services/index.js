3716f3806234582cc638fcaec2b779c807062fb7
darkcode <atebederussel@gmail.com>
2022-04-03 10:40:33 +0100

    ajout du composant des posts recents avec ses fonctions de post recent et post similaires

--- services/index.js
+++ services/index.js
@@ -37,4 +37,54 @@ export const getPosts = async () => {
     const result = await request(graphqlAPI, query)
     
     return result.postsConnection.edges
+};
+
+export const getRecentPosts = async () => {
+    const query = gql`
+    query GetPostDetails {
+      posts(
+        orderBy: createdAt_DESC
+        last: 3
+      ){
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+      
+      
+      
+      ` 
+    const result = await request(graphqlAPI, query);
+    
+    return result.posts; 
+}
+
+export const getSimilarPosts = async () => {
+    const query = gql`
+    query GetPostdetails($slug: String!, $categories: [String!]){
+      posts(
+        where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
+        last: 3
+      ){
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+      
+      
+      
+      
+      `
+
+    const result = await request(graphqlAPI, query);
+
+    return result.posts;
 }
