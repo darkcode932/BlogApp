93c4a1e9039c83ad80698efcaf9ae60eeb88f2ba
darkcode932 <atebederussel@gmail.com>
2022-12-02 11:11:23 +0100

    add linter, prettier and cypress for test

---
+++ cypress/component/Comments.cy.js
@@ -0,0 +1,5 @@
+describe('Comments.cy.js', () => {
+  it('playground', () => {
+    // cy.mount()
+  })
+})
---
+++ cypress/component/Navigation.cy.js
@@ -0,0 +1,5 @@
+describe('Navigation.cy.js', () => {
+  it('playground', () => {
+    // cy.mount()
+  })
+})
